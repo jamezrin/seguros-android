9bf99fa0a5063671bfaa3fd98759e3e29229bc88
Jaime <mrjaime1999@gmail.com>
2020-02-07 12:38:20 +0100

    commit inicial

---
+++ base.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS seguros_android;
+CREATE DATABASE IF NOT EXISTS seguros_android;
+USE seguros_android;
+
+CREATE TABLE cliente (
+    dni VARCHAR(9) NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    apellidos VARCHAR(100) NOT NULL,
+    direccion VARCHAR(255) NOT NULL,
+    telefono VARCHAR(9) NOT NULL,
+    PRIMARY KEY (dni)
+);
+
+CREATE TABLE tipo_seguro (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    tipo VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE seguro (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    fecha_alta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_baja TIMESTAMP,
+    dni_cliente VARCHAR(9) NOT NULL,
+    id_tipo_seguro INTEGER NOT NULL,
+    CONSTRAINT fk_cliente_seguro FOREIGN KEY (dni_cliente) REFERENCES cliente (dni)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_tipo_seguro FOREIGN KEY (id_tipo_seguro) REFERENCES tipo_seguro (id),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO tipo_seguro (tipo) VALUES ("Vida"), ("Vivienda"), ("Coche");
