7d0935f738fca719af11ec0b66b96846aff80bb0
Jaime <mrjaime1999@gmail.com>
2020-02-14 11:06:24 +0100

    Creado POJO de Cliente

--- SegurosAndroid/app/src/main/java/me/android/seguros/modelos/Cliente.java
+++ SegurosAndroid/app/src/main/java/me/android/seguros/modelos/Cliente.java
@@ -1,5 +1,92 @@
 package me.android.seguros.modelos;
 
+import java.util.Objects;
+
 public class Cliente {
-    private String dni;
+    private final String dni;
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    private String telefono;
+    private String contrasena;
+    private boolean borrado;
+
+    public Cliente(String dni) {
+        this.dni = dni;
+    }
+
+    public Cliente(String dni, String nombre, String apellidos, String direccion, String telefono, String contrasena, boolean borrado) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.contrasena = contrasena;
+        this.borrado = borrado;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public boolean isBorrado() {
+        return borrado;
+    }
+
+    public void setBorrado(boolean borrado) {
+        this.borrado = borrado;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", contrasena='" + contrasena + '\'' +
+                ", borrado=" + borrado +
+                '}';
+    }
 }
