c77cdfa6e886bbf51a7c83b41002ef569bcf93c5
Jaime <mrjaime1999@gmail.com>
2020-02-18 09:50:23 +0100

    mis cambios

--- SegurosAndroid/app/src/main/java/me/android/seguros/MainActivity.java
+++ SegurosAndroid/app/src/main/java/me/android/seguros/MainActivity.java
@@ -2,13 +2,25 @@ package me.android.seguros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import me.android.seguros.actividades.ActividadAdmin;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ActividadAdmin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
