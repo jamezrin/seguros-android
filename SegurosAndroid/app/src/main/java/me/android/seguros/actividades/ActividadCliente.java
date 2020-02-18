c2c8984d8cfdad7a5bfccdcb0540008c8e605f68
Jaime <mrjaime1999@gmail.com>
2020-02-18 09:06:58 +0100

    Añadidas actividades

---
+++ SegurosAndroid/app/src/main/java/me/android/seguros/actividades/ActividadCliente.java
@@ -0,0 +1,16 @@
+package me.android.seguros.actividades;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import me.android.seguros.R;
+
+public class ActividadCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_cliente);
+    }
+}
