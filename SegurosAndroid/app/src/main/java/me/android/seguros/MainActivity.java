c49388e6f342b811bb1bc555690372c89fa24e4a
mcanalejas <miguelcana99@gmail.com>
2020-02-18 10:12:19 +0100

    F EN EL CHAT

--- SegurosAndroid/app/src/main/java/me/android/seguros/MainActivity.java
+++ SegurosAndroid/app/src/main/java/me/android/seguros/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import me.android.seguros.actividades.ActividadAdmin;
+import me.android.seguros.actividades.ActividadLogin;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), ActividadAdmin.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.button7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ActividadLogin.class);
                 startActivity(intent);
             }
         });
